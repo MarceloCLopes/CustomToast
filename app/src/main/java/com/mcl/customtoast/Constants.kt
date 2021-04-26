483c272acbcbeb6ea1d99fa8cf6ac9892ea31454
MarceloCLopes <marc01mcl@gmail.com>
2021-04-26 10:30:02 -0300

    Created project kotlin custom toast

---
+++ app/src/main/java/com/mcl/customtoast/Constants.kt
@@ -0,0 +1,8 @@
+package com.mcl.customtoast
+
+object Constants {
+    const val TOAST_TYPE_SUCCESS = 1
+    const val TOAST_TYPE_WARNING = 2
+    const val TOAST_TYPE_ERROR = 3
+
+}
---
+++ app/src/main/java/com/mcl/customtoast/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.mcl.customtoast
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Message
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSuccessToast.setOnClickListener {
+            showToast(Constants.TOAST_TYPE_SUCCESS, "Success Toast")
+        }
+
+        buttonWarningToast.setOnClickListener {
+            showToast(Constants.TOAST_TYPE_WARNING, "Warning Toast")
+        }
+
+        buttonErrorToast.setOnClickListener {
+            showToast(Constants.TOAST_TYPE_ERROR, "Error Toast")
+        }
+    }
+
+    private fun showToast(toastType: Int, message: CharSequence){
+        val toastView = layoutInflater.inflate(
+            R.layout.layout_toast,
+            findViewById(R.id.layoutToastContainer)
+        )
+
+        val toastContainer = toastView.findViewById<LinearLayout>(R.id.layoutToastContainer)
+        val imageIcon = toastView.findViewById<ImageView>(R.id.imageIcon)
+        val textMessage = toastView.findViewById<TextView>(R.id.textMessage)
+
+        when(toastType){
+
+            Constants.TOAST_TYPE_SUCCESS -> {
+                toastContainer.setBackgroundResource(R.drawable.toast_success_background)
+                imageIcon.setImageResource(R.drawable.ic_success)
+                textMessage.text = message
+            }
+
+            Constants.TOAST_TYPE_WARNING -> {
+                toastContainer.setBackgroundResource(R.drawable.toast_warning_background)
+                imageIcon.setImageResource(R.drawable.ic_warning)
+                textMessage.text = message
+            }
+
+            Constants.TOAST_TYPE_ERROR -> {
+                toastContainer.setBackgroundResource(R.drawable.toast_error_background)
+                imageIcon.setImageResource(R.drawable.ic_error)
+                textMessage.text = message
+            }
+        }
+
+        with(Toast(applicationContext)){
+            duration = Toast.LENGTH_SHORT
+            view = toastView
+            show()
+        }
+    }
+}
